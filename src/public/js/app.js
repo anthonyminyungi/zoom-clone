0f5deae643b7c60663879017ed0de52d8eb43a80
anthonymin <dbstnsdl12@naver.com>
2022-07-31 03:43:28 +0900

    Done #3.11 Data Channels

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,6 +13,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 /** @type{RTCPeerConnection} */ let myPeerConnection;
+/** @type{RTCDataChannel} */ let myDataChannel;
 
 async function getCameras() {
   try {
@@ -122,14 +123,25 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
+// First Peer
 socket.on("welcome", async () => {
+  myDataChannel = myPeerConnection.createDataChannel("chat");
+  myDataChannel.addEventListener("message", (event) => console.log(event.data));
+  console.log("made data channel");
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log("sent the offer");
   socket.emit("offer", offer, roomName);
 });
 
+// Second Peer
 socket.on("offer", async (offer) => {
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener("message", (event) =>
+      console.log(event.data)
+    );
+  });
   console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
